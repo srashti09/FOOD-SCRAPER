4dc938356a33bd2f01846098a4c21897a5194992
srashti09 <srashtisaxena79@gmail.com>
2024-05-29 11:32:56 +0530

    updated scraper.js in api

--- api/scraper.js
+++ api/scraper.js
@@ -10,9 +10,22 @@ const headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 };
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://srashtisaxena09:B3i79IjFI0LlQ7EN@cluster0.yi8w1li.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const DATABASE_NAME = 'testDatabase';
 const COLLECTION_NAME = 'testCollection';
+
+console.log("MONGODB_URI:", MONGODB_URI);  // Debugging line
+
+let client;
+
+async function connectToMongoDB() {
+    if (!client) {
+        client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await client.connect();
+        console.log("Connected to MongoDB");
+    }
+    return client;
+}
 
 export default async function scrapeAndStore() {
     try {
@@ -32,20 +45,13 @@ export default async function scrapeAndStore() {
                 const scriptContent = JSON.parse(scriptTag.html().trim());
 
                 // Connect to MongoDB
-                const client = new MongoClient(MONGODB_URI);
-
-                await client.connect();
-                console.log("Connected to MongoDB");
-
+                const client = await connectToMongoDB();
                 const db = client.db(DATABASE_NAME);
                 const collection = db.collection(COLLECTION_NAME);
 
                 // Insert data into MongoDB
                 await collection.insertOne(scriptContent);
                 console.log("Data inserted into MongoDB");
-
-                // Close the connection
-                await client.close();
             } else {
                 console.log("Error: Script tag with id='__NEXT_DATA__' not found.");
             }
